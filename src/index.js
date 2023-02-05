983b9b8582aa6b953b0d848a4df1329812cec543
Sergey <telmy@yandex.ru>
2023-02-05 23:33:29 +0300

    fix: fix task Towel Sort

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-
-    return [];
+    // 1. если матрица не задана
+    if (matrix == undefined) {
+        return [];
+    }
+    else {
+        // 2.перевернуть каждый 2й элемент
+        for (i = 1; i < matrix.length; i += 2) {
+            matrix[i].reverse()
+        }
+        // 3. опустить все элементы на общий уровень
+        return matrix.flat()
+    }
 }
+
